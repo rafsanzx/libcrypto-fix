ad7b1ad4156c3e6240e014c38ffcb97ee91b6f36
rafsanzx <128445052+rafsanzx@users.noreply.github.com>
2024-01-16 20:01:28 +0600

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd /tmp
+
+##for debian need to add kali repo. 
+
+sudo echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+
+sudo apt install gnupg -y
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ED444FF07D8D0BF6
+
+sudo apt --fix-broken install -y
+
+cp /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d
+
+sudo apt update
+
+##done for debian
+
+
+apt download libcrypt1
+
+dpkg-deb -x libcrypt1*.deb ./
+
+##for aarch64 or arm
+
+sudo cp -av lib/aarch64-linux-gnu/* /lib/aarch64-linux-gnu/
+
+##for x86_64
+
+##sudo cp -av lib/x86_64-linux-gnu/* /lib/x86_64-linux-gnu/
+
+sudo apt update
